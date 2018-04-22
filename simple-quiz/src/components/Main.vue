4ca2f101d70d142a3420a56662d524814104c568
Denis Anchugov <denis.anchugov@outlook.com>
2018-04-22 19:38:22 +0200

    Show wrong answers after quiz is completed.

--- simple-quiz/src/components/Main.vue
+++ simple-quiz/src/components/Main.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
-    <input type="file" v-if="values.length === 0" @change="loadFile($event)">
-    <h3 v-if="values.length !== 0">{{currentIndex + 1}}/{{values.length}}</h3>
-    <h1 v-if="currentIndex !== null">{{values[currentIndex][0]}}</h1>
-    <input type="text" v-if="currentIndex !== null" @keyup="handleKeyboard($event)">
+    <div v-if="currentIndex + 1 !== values.length">
+      <input type="file" v-if="values.length === 0" @change="loadFile($event)">
+      <h3 v-if="values.length !== 0">{{currentIndex + 1}}/{{values.length}}</h3>
+      <h1 v-if="currentIndex !== null">{{values[currentIndex][0]}}</h1>
+      <input type="text" v-if="currentIndex !== null" @keyup="handleKeyboard($event)">
+    </div>
+    <div v-if="currentIndex + 1 === values.length">
+      <h3 v-if="values.length !== 0"><h2>Result:</h2> {{values.length - wrongAnswers.length}}/{{values.length}}</h3>
+      <br>
+      <h2>
+        Incorrect:  
+      </h2>
+      <ul>
+        <li v-for="valueIndex in wrongAnswers">
+          <h3>{{values[valueIndex][0]}} - {{values[valueIndex][1]}}</h3>
+        </li>
+      </ul>
+    </div>
   </div>
+  
 </template>
 
 <script>
@@ -22,7 +37,7 @@ export default {
     },
     handleKeyboard(event) {
       if (event.keyCode === 13) { 
-        if (this.checkAnswer(event.target.value) === 0) {
+        if (this.checkAnswer(event.target.value) !== 0) {
           this.wrongAnswers.push(this.currentIndex);
         }
         event.target.value = '';
@@ -35,3 +50,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  ul {
+    list-style: none;
+  }
+</style>
